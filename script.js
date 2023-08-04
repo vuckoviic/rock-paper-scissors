da6128f011c87cf69e293c360f8be5f3eb2e7047
vuckoviic <vuckovica64@gmail.com>
2023-08-04 16:10:58 +0200

    Declared and defined playerScore and computerScore
    after every round score is changed, but it restarts, I need to fix that

--- script.js
+++ script.js
@@ -18,39 +18,55 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = prompt('Select you weapon: ');
     computerSelection = result;
 
+    let playerScore = 0;
+    let computerScore = 0;
+
     // options for choosing rock
     if (playerSelection == 'Rock' && computerSelection == 'Scissors'){
         console.log("You won! Rock beats scissors!");
+        playerScore = ++playerScore;
     }
     else if (playerSelection == 'Rock' && computerSelection == 'Rock'){
         console.log("Tied! Rock is equal to rock!");
+        playerScore = ++playerScore;
+        computerScore = ++computerScore;
     }
     else if (playerSelection == 'Rock' && computerSelection == 'Paper'){
         console.log("You lost! Paper beats rock!");
+        computerScore = ++computerScore;
     }
 
     //options for choosing paper
     if (playerSelection == 'Paper' && computerSelection == 'Rock'){
         console.log("You won! Paper beats rock!");
+        playerScore = ++playerScore;
     }
     else if (playerSelection == 'Paper' && computerSelection == 'Paper'){
         console.log("Tied! Paper is equal to paper!");
+        playerScore = ++playerScore;
+        computerScore = ++computerScore;
     }
     else if (playerSelection == 'Paper' && computerSelection == 'Scissors'){
         console.log("You lost! Scissors beats paper!")
+        computerScore = ++computerScore;
     }
 
     //options for choosing scissors
 
     if(playerSelection == 'Scissors' && computerSelection == 'Paper'){
         console.log("You won! Scissors beat paper!");
+        playerScore = ++playerScore;
     }
     else if (playerSelection == 'Scissors' && computerSelection == 'Scissors'){
         console.log("Tied! Scissors are equal to scissors!");
+        playerScore = ++playerScore;
+        computerScore = ++computerScore;
     }
     else if (playerSelection == 'Scissors' && computerSelection == 'Rock'){
         console.log("You lost! Rock beats scissors!");
+        computerScore = ++computerScore;
     }
+    console.log(`Score is now: ${playerScore} : ${computerScore}`);
 }
    
 function game() {
