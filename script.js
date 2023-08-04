076e42e13ece5f1b8c3dfc942819ab2343e9da83
vuckoviic <vuckovica64@gmail.com>
2023-08-04 16:29:45 +0200

    Made playerSelection case-insensitive
    added toUpperCase to playerSelection prompt

--- script.js
+++ script.js
@@ -18,51 +18,51 @@ function playRound(playerSelection, computerSelection) {
     
     const result = getComputerChoice(choice);
     
-    playerSelection = prompt('Select you weapon: ');
+    playerSelection = prompt('Select you weapon: ').toUpperCase();
     computerSelection = result;
 
     // options for choosing rock
-    if (playerSelection == 'Rock' && computerSelection == 'Scissors'){
+    if (playerSelection == 'ROCK' && computerSelection == 'Scissors'){
         console.log("You won! Rock beats scissors!");
         playerScore = ++playerScore;
     }
-    else if (playerSelection == 'Rock' && computerSelection == 'Rock'){
+    else if (playerSelection == 'ROCK' && computerSelection == 'Rock'){
         console.log("Tied! Rock is equal to rock!");
         playerScore = ++playerScore;
         computerScore = ++computerScore;
     }
-    else if (playerSelection == 'Rock' && computerSelection == 'Paper'){
+    else if (playerSelection == 'ROCK' && computerSelection == 'Paper'){
         console.log("You lost! Paper beats rock!");
         computerScore = ++computerScore;
     }
 
     //options for choosing paper
-    if (playerSelection == 'Paper' && computerSelection == 'Rock'){
+    if (playerSelection == 'PAPER' && computerSelection == 'Rock'){
         console.log("You won! Paper beats rock!");
         playerScore = ++playerScore;
     }
-    else if (playerSelection == 'Paper' && computerSelection == 'Paper'){
+    else if (playerSelection == 'PAPER' && computerSelection == 'Paper'){
         console.log("Tied! Paper is equal to paper!");
         playerScore = ++playerScore;
         computerScore = ++computerScore;
     }
-    else if (playerSelection == 'Paper' && computerSelection == 'Scissors'){
+    else if (playerSelection == 'PAPER' && computerSelection == 'Scissors'){
         console.log("You lost! Scissors beats paper!")
         computerScore = ++computerScore;
     }
 
     //options for choosing scissors
 
-    if(playerSelection == 'Scissors' && computerSelection == 'Paper'){
+    if(playerSelection == 'SCISSORS' && computerSelection == 'Paper'){
         console.log("You won! Scissors beat paper!");
         playerScore = ++playerScore;
     }
-    else if (playerSelection == 'Scissors' && computerSelection == 'Scissors'){
+    else if (playerSelection == 'SCISSORS' && computerSelection == 'Scissors'){
         console.log("Tied! Scissors are equal to scissors!");
         playerScore = ++playerScore;
         computerScore = ++computerScore;
     }
-    else if (playerSelection == 'Scissors' && computerSelection == 'Rock'){
+    else if (playerSelection == 'SCISSORS' && computerSelection == 'Rock'){
         console.log("You lost! Rock beats scissors!");
         computerScore = ++computerScore;
     }
