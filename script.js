ea00103e9e69ffa932d5c54845962d3fefa96641
vuckoviic <vuckovica64@gmail.com>
2023-08-15 16:33:23 +0200

    Added textContent which is representing round winner thourght paragraph

--- script.js
+++ script.js
@@ -3,11 +3,7 @@ let computerScore = 0;
 
 let playerSelection = '';
 
-
-
-
-
-
+const rndWnr = document.getElementById('rndWnr');
     
 function playRound(computerSelection) {
 
@@ -31,42 +27,52 @@ function playRound(computerSelection) {
 
     // options for choosing rock
     if (playerSelection == 'ROCK' && computerSelection == 'Scissors'){
+        rndWnr.textContent = "You won! Rock beats scissors!";
         console.log("You won! Rock beats scissors!");
         playerScore = ++playerScore;
     }
     else if (playerSelection == 'ROCK' && computerSelection == 'Rock'){
         console.log("Tied! Rock is equal to rock!");
+        rndWnr.textContent = 'You won! Rock beats scissors!';
     }
     else if (playerSelection == 'ROCK' && computerSelection == 'Paper'){
         console.log("You lost! Paper beats rock!");
         computerScore = ++computerScore;
+        rndWnr.textContent = 'You lost! Paper beats rock!';
     }
 
     //options for choosing paper
     if (playerSelection == 'PAPER' && computerSelection == 'Rock'){
         console.log("You won! Paper beats rock!");
         playerScore = ++playerScore;
+        rndWnr.textContent = 'You won! Paper beats rock!';
     }
     else if (playerSelection == 'PAPER' && computerSelection == 'Paper'){
         console.log("Tied! Paper is equal to paper!");
+        rndWnr.textContent = 'Tied! Paper is equal to paper!';
     }
     else if (playerSelection == 'PAPER' && computerSelection == 'Scissors'){
         console.log("You lost! Scissors beats paper!")
         computerScore = ++computerScore;
+        rndWnr.textContent = 'You lost! Scissors beats paper!';
     }
+    
 
     //options for choosing scissors
 
     if(playerSelection == 'SCISSORS' && computerSelection == 'Paper'){
         console.log("You won! Scissors beat paper!");
         playerScore = ++playerScore;
+        rndWnr.textContent = 'You won! Scissors beat paper!';
     }
     else if (playerSelection == 'SCISSORS' && computerSelection == 'Scissors'){
         console.log("Tied! Scissors are equal to scissors!");
+        rndWnr.textContent = 'Tied! Scissors are equal to scissors!';
     }
     else if (playerSelection == 'SCISSORS' && computerSelection == 'Rock'){
         console.log("You lost! Rock beats scissors!");
         computerScore = ++computerScore;
+        rndWnr.textContent = 'You lost! Rock beats scissors!';
     }
     console.log(`Score is now: ${playerScore} : ${computerScore}`);
 }
